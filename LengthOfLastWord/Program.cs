a7598a8f6c54ab81a7439f610d5d2820c7c0585a
hananiah-linde <57416903+hananiah-linde@users.noreply.github.com>
2022-04-05 15:30:54 -0500

    Added LengthOfLastWork solution

---
+++ LengthOfLastWord/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://leetcode.com/problems/length-of-last-word/ for more information
+Console.WriteLine("Hello, World!");
+
+var result = LengthOfLastWord("   fly me   to   the moon  ");
+Console.WriteLine(result);
+
+Console.ReadLine();
+
+int LengthOfLastWord(string s)
+{
+    //Trim trailing and leading whitespace.
+    //Use split to add each word/whitespace into array
+    //Use LastOrDefault().length to get length of last word
+    return s.Trim().Split().LastOrDefault().Length;
+}
