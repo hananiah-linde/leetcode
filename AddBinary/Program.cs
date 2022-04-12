6e318450820057fb27f951be9d31cf8efb7398d4
hananiah-linde <57416903+hananiah-linde@users.noreply.github.com>
2022-04-12 15:31:37 -0500

    Add solution for AddBinary and Sqrtx

---
+++ AddBinary/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://leetcode.com/problems/add-binary/ for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var result = AddBinary("111110101110111101011111", "1011"); //10 + 11 return 010101
+
+Console.WriteLine(result);
+
+Console.ReadLine();
+
+string AddBinary(string a, string b)
+{
+    if (a.Length < 23 && b.Length < 23) return Convert.ToString((Convert.ToInt32(a, 2) + Convert.ToInt32(b, 2)), 2);
+
+    var carry = 0;
+    var builder = new StringBuilder();
+
+    var aLength = a.Length - 1;
+    var bLength = b.Length - 1;
+
+    int aVal, bVal, val;
+    while (aLength >= 0 || bLength >= 0)
+    {
+        aVal = aLength >= 0 ? a[aLength] - '0' : 0;
+        bVal = bLength >= 0 ? b[bLength] - '0' : 0;
+
+        val = aVal + bVal + carry;
+        builder.Insert(0, val & 1);
+
+        carry = val >> 1;
+
+        aLength--;
+        bLength--;
+    }
+
+    if (carry >= 1)
+    {
+        builder.Insert(0, carry);
+    }
+
+    return builder.ToString();
+
+}
---
+++ Sqrtx/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://leetcode.com/problems/sqrtx/ for more information
+Console.WriteLine("Hello, World!");
+
+var result = MySqrt(300);
+Console.WriteLine(result);
+
+Console.ReadLine();
+
+int MySqrt(int x)
+{
+    //return (int)Math.Sqrt(x); Real world soltion
+    if (x == 0) return 0;
+    if (x == 1) return 1;
+    long min = 0;
+    long max = x;
+
+    while (min < max)
+    {
+        long mid = min + (max - min) / 2;
+        long candidate = mid * mid;
+
+        if (candidate == x)
+            return (int)mid;
+        else if (candidate > x)
+        {
+            max = mid;
+        }
+        else
+        {
+            min = mid + 1;
+        }
+    }
+
+    return (int)min - 1;
+}
